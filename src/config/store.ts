929f282b4ab46fbdcbb85343a4010c81ba6e94cc
ronn <ronnie-filth@hotmail.com>
2023-09-04 13:13:59 +0200

    Roll back redux on profile

--- src/config/store.ts
+++ src/config/store.ts
@@ -1,11 +1,9 @@
 import feedReducer from "../domain/home/modules/feedSlice"
 import {configureStore} from "@reduxjs/toolkit"
-import profileReducer from "../domain/profile/modules/profileSlice";
 
 export const store = configureStore({
     reducer: {
         feed: feedReducer,
-        profile: profileReducer
     },
     middleware: getDefaultMiddleware => getDefaultMiddleware()
 })
