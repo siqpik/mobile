2285a34178166720e7ab7f7fc4284f6468e89820
ronn <ronnie-filth@hotmail.com>
2023-09-05 12:50:26 +0200

    Picture component to hok

--- src/domain/pictureview/Picture.js
+++ src/domain/pictureview/Picture.js
@@ -1,67 +1,64 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Image, ScrollView, Text, TouchableOpacity, View} from "react-native"
 import {styles} from "./style/styles"
-import {post} from '../service/ApiService';
 import PagerView from "react-native-pager-view";
+import {post} from "../service/ApiService";
 
-export class Picture extends Component {
+export default props => {
+    const {posts, username, index, actualUser, deletePost} = props.route.params;
 
-    constructor(props) {
-        super(props)
-        console.log(this.props.route.params)
-    }
+    const changeProfilePic = pidId =>
+        post('/profile/changeProfilePic/' + pidId)
+            .then(resp => {
+                if (resp.status === 200) {
+                    console.log(resp.status);
+                }
+            }).catch(error => alert(error));
 
-    render() {
-        const {posts, username, index, actualUser, deletePost} = this.props.route.params;
-        return (
-            <View style={styles.container}>
+    const getPics = (posts, actualUser, username, deletePost) => posts.map((pic, index) =>
+        <View key={index + 'pictureView'}>
+            <Text style={styles.userTop}>{username}</Text>
 
-                <PagerView style={styles.takenPic} initialPage={index} showPageIndicator={false}
-                           orientation={'horizontal'}>
-                    {getPics(posts, actualUser, username, deletePost)}
-                </PagerView>
-            </View>
-        )
-    }
-}
+            <Image source={{uri: pic.mediaUrl}} style={styles.pic}/>
+            {actualUser ?
+                <View style={styles.buttonContainer} style={styles.titleContainer}>
 
-const changeProfilePic = pidId =>
-    post('/profile/changeProfilePic/' + pidId)
-        .then(resp => {
-            if (resp.status === 200) {
-                console.log(resp.status);
-            }
-        }).catch(error => alert(error));
-
-const getPics = (posts, actualUser, username, deletePost) => posts.map((pic, index) =>
-    <View key={index + 'pictureView'}>
-        <Text style={styles.userTop}>{username}</Text>
-
-        <Image source={{uri: pic.mediaUrl}} style={styles.pic}/>
-        {actualUser ?
-            <View style={styles.buttonContainer} style={styles.titleContainer}>
-
-                {/*<TouchableOpacity onPress={() => changeProfilePic(pic.id)}
+                    {/*<TouchableOpacity onPress={() => changeProfilePic(pic.id)}
                                   style={styles.delete_button}>
                     <Text>Make Profile Picture</Text>
                 </TouchableOpacity>*/}
-                {<TouchableOpacity onPress={() => deletePost()(pic.id)} style={styles.delete_button}>
-                    <Text>Delete</Text>
-                </TouchableOpacity>}
-            </View> :
-            <View></View>
-        }
-        <ScrollView style={styles.commentContainer} alwaysBounceHorizontal={false}>
-            <Text style={styles.date}>{pic.date}</Text>
-            {/**getComments(pic)*/}
-        </ScrollView>
+                    {<TouchableOpacity onPress={() => {
+                        props.navigation.navigate("Profile")
+                        deletePost()(pic.id)
+                    }}
+                                       style={styles.delete_button}>
+                        <Text>Delete</Text>
+                    </TouchableOpacity>}
+                </View> :
+                <View></View>
+            }
+            <ScrollView style={styles.commentContainer} alwaysBounceHorizontal={false}>
+                <Text style={styles.date}>{pic.date}</Text>
+                {/**getComments(pic)*/}
+            </ScrollView>
 
-    </View>
-)
+        </View>
+    )
 
-const getComments = (pic) => pic.comments.map((post, index) =>
-    <View key={index} style={styles.comments}>
-        <Text style={styles.user}>{post.userName}</Text>
-        <Text style={styles.comment}>{post.comments}</Text>
-    </View>
-)
+    const getComments = (pic) => pic.comments.map((post, index) =>
+        <View key={index} style={styles.comments}>
+            <Text style={styles.user}>{post.userName}</Text>
+            <Text style={styles.comment}>{post.comments}</Text>
+        </View>
+    )
+
+    return (
+        <View style={styles.container}>
+
+            <PagerView style={styles.takenPic} initialPage={index} showPageIndicator={false}
+                       orientation={'horizontal'}>
+                {getPics(posts, actualUser, username, deletePost)}
+            </PagerView>
+        </View>
+    )
+}
