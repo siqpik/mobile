f45b8963cf798b7e38ff20cb500c3c584c2c1b88
ronn <ronnie-filth@hotmail.com>
2024-10-11 17:40:57 -0500

    Remove reminder notification when a user logs out

--- src/domain/service/AuthenticationService.ts
+++ src/domain/service/AuthenticationService.ts
@@ -1,6 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import AuthenticationRequest from './model/AuthenticationRequest';
 import {authenticatedRequest, genericPost} from './ApiService';
+import notifee from "@notifee/react-native";
 
 export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'USER_NAME_SESSION_ATTRIBUTE_NAME';
 export const TOKEN_SESSION = 'TOKEN_SESSION_ATTRIBUTE_NAME';
@@ -21,5 +22,7 @@ export const logout = () => authenticatedRequest('/logout', 'GET')
         } else {
             AsyncStorage.removeItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
             AsyncStorage.removeItem(TOKEN_SESSION)
+
+            notifee.cancelNotification('open-app-reminder').then()
         }
     })
